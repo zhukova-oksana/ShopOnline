569baccab5f358c3f7caef796f3b0ca77d90699b
zhukova_oksana <ksy2905@gmail.com>
2022-11-02 09:23:30 +0300

    implementation of the embeddable timer code

--- script/script.js
+++ script/script.js
@@ -34,8 +34,11 @@ const timer = deadline => {
 
   const getTimeRemaining = () => {
     const dateStop = new Date(deadline).getTime();
+    const dataTimeZone = new Date(deadline).getTimezoneOffset();
+    const dateStopNew = dateStop - ((dataTimeZone + 180) * 60 * 1000);
+
     const dateNow = Date.now();
-    const timeRemaining = dateStop - dateNow;
+    const timeRemaining = dateStopNew - dateNow;
 
     const seconds = Math.floor(timeRemaining / 1000 % 60);
     const minutes = Math.floor(timeRemaining / 1000 / 60 % 60);
@@ -43,38 +46,90 @@ const timer = deadline => {
     const days = Math.floor(timeRemaining / 1000 / 60 / 60 / 24);
 
     return {timeRemaining, seconds, minutes, hours, days}
+  };
+
+  const createTimer = (obj, timerContainer) => {
+    if (obj.day < 1) {
+      timerContainer.innerHTML = `
+      <p class="period__text">До конца акции:</p>
+      <div class="time">
+        <p class="time__text time__text_day visually-hidden"><span class="time__number time__number_day">${obj.day}</span> ${obj.dayWord}</p>
+        <p class="time__text time__text_hour"><span class="time__number time__number_hour">${obj.hour}</span> ${obj.hourWord}</p>
+        <p class="time__text time__text_min"><span class="time__number time__number_min">${obj.minute}</span> ${obj.minuteWord}</p>
+        <p class="time__text time__text_sec"><span class="time__number time__number_sec">${obj.second}</span> ${obj.secondWord}</p>
+      </div>`;
+    } else {
+      timerContainer.innerHTML = `
+      <p class="period__text">До конца акции:</p>
+      <div class="time">
+        <p class="time__text time__text_day"><span class="time__number time__number_day">${obj.day}</span> ${obj.dayWord}</p>
+        <p class="time__text time__text_hour"><span class="time__number time__number_hour">${obj.hour}</span> ${obj.hourWord}</p>
+        <p class="time__text time__text_min"><span class="time__number time__number_min">${obj.minute}</span> ${obj.minuteWord}</p>
+        <p class="time__text time__text_sec visually-hidden"><span class="time__number time__number_sec">${obj.second}</span> ${obj.secondWord}</p>
+      </div>`;
+    }
+
+    return timerContainer;
   }
+
+  const timerContainer = document.querySelectorAll('[data-deadline]');
+
+  const twoNumbers = (num) => ((num < 10) ? '0' + num : num);
+
   const start = () => {
     const timer = getTimeRemaining();
 
-    timerNumberDay.textContent = timer.days + declinationWork(timer.days, daysWords);
-    // timerNumberDay.after(declinationWork(timer.days, daysWords));
-    timerNumberHour.textContent = timer.hours + declinationWork(timer.hours, hoursWords);
-    // timerNumberHour.after(declinationWork(timer.hours, hoursWords));
-    timerNumberMin.textContent = timer.minutes + declinationWork(timer.minutes, minutesWords);
-    // timerNumberMin.after(declinationWork(timer.minutes, minutesWords));
-    timerNumberSec.textContent = timer.seconds + declinationWork(timer.seconds, secondsWords);
-    // timerNumberSec.after(declinationWork(timer.seconds, secondsWords));
+    let day = timer.days;
+    const dayWord = declinationWork(day, daysWords);
+
+    let hour = twoNumbers(timer.hours);
+    const hourWord = declinationWork(hour, hoursWords);
+
+    let minute = twoNumbers(timer.minutes);
+    const minuteWord = declinationWork(minute, minutesWords);
+
+    let second = twoNumbers(timer.seconds);
+    const secondWord = declinationWork(second, secondsWords);
+
+    let timerInfo = {
+      day,
+      dayWord,
+      hour,
+      hourWord,
+      minute,
+      minuteWord,
+      second,
+      secondWord
+    }
 
     let intervalID;
-    if (timer.days < 1) {
+    if (timerInfo.day < 1) {
       intervalID = setTimeout(start, 1000);
-      timerBlockDay.style.display = 'none';
-      timerBlockSec.style.display = 'block';
     } else {
       intervalID = setTimeout(start, 60000);
     }
+
     if (timer.timeRemaining <= 0) {
       clearTimeout(intervalID);
-      timerNumberDay.textContent = '0';
-      timerNumberHour.textContent = '00';
-      timerNumberMin.textContent = '00';
-      timerNumberSec.textContent = '00';
+      timerInfo.day = '00';
+      timerInfo.hour = '00';
+      timerInfo.minute = '00';
+      timerInfo.second = '00';
+      const period = document.querySelector('.period');
+      period.classList.add('visually-hidden');
     }
+
+    const time = createTimer(timerInfo, timerContainer);
+    const period = document.querySelector('.period');
+    period.innerHTML = time.innerHTML;
+
+    return period;
   };
 
   start();
-
 };
-timer('2022/11/29 21:47');
+
+const deadline = document.querySelector('.period');
+timer(deadline.getAttribute('data-deadline'));
+
 }
